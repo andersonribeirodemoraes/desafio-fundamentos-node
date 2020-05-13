45d203a417997753e419e6801b8573baec080ba2
Anderson Ribeiro <anderson.moraes@m2msolutions.com.br>
2020-05-13 10:40:56 -0300

    implementacao da transacao

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,45 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce(
+      (acumulador: number, valorAtual: Transaction) => {
+        return (
+          acumulador + (valorAtual.type === 'income' ? valorAtual.value : 0)
+        );
+      },
+      0,
+    );
+
+    const outcome = this.transactions.reduce(
+      (acumulador: number, valorAtual: Transaction) => {
+        return (
+          acumulador + (valorAtual.type === 'outcome' ? valorAtual.value : 0)
+        );
+      },
+      0,
+    );
+
+    const total = income - outcome;
+
+    const balance: Balance = {
+      income,
+      outcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
